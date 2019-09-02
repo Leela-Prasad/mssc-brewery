b575bde9d26aa85f3ebc9c52e9d5e9feca98ec7c
Leela <leela.jagu@gmail.com>
2019-09-02 17:10:31 +0530

    Generating Mapper Classes using Mapstruct - closes #2

---
+++ src/main/java/guru/springframework/msscbrewery/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbrewery.mappers;
+
+import guru.springframework.msscbrewery.domain.Beer;
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    Beer beerDtoToBeer(BeerDto beerDto);
+
+    BeerDto beerToBeerDto(Beer beer);
+}
